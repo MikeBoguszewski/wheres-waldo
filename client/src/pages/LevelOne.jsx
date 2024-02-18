e369c9f2d037190da7cd483e4bd2b820fce4261f
MikeBoguszewski <133729602+MikeBoguszewski@users.noreply.github.com>
2024-02-18 12:25:07 -0600

    Update routes to match deployed URL

--- client/src/pages/LevelOne.jsx
+++ client/src/pages/LevelOne.jsx
@@ -16,7 +16,7 @@ export default function LevelOne() {
   useEffect(() => {
     const fetchPositions = async () => {
       try {
-        const response = await fetch("http://localhost:3000/levels/1");
+        const response = await fetch("https://wheres-waldo-8za6.onrender.com/levels/1");
         const data = await response.json();
         setCharacters(data);
       } catch (error) {
@@ -25,7 +25,7 @@ export default function LevelOne() {
     };
     const fetchScores = async () => {
       try {
-        const response = await fetch("http://localhost:3000/scores/1");
+        const response = await fetch("https://wheres-waldo-8za6.onrender.com/scores/1");
         const data = await response.json();
         setScores(data);
       } catch (error) {
--- client/src/pages/LevelThree.jsx
+++ client/src/pages/LevelThree.jsx
@@ -16,7 +16,7 @@ export default function LevelThree() {
   useEffect(() => {
     const fetchPositions = async () => {
       try {
-        const response = await fetch("http://localhost:3000/levels/3");
+        const response = await fetch("https://wheres-waldo-8za6.onrender.com/levels/3");
         const data = await response.json();
         setCharacters(data);
       } catch (error) {
@@ -25,7 +25,7 @@ export default function LevelThree() {
     };
     const fetchScores = async () => {
       try {
-        const response = await fetch("http://localhost:3000/scores/3");
+        const response = await fetch("https://wheres-waldo-8za6.onrender.com/scores/3");
         const data = await response.json();
         setScores(data);
       } catch (error) {
